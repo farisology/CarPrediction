bec28d96d966386d9a5fbf2f6099195b0e189fb5
fares hasan <farishassan2012@gmail.com>
2018-09-16 21:09:11 +0800

    API

--- carapi.py
+++ carapi.py
@@ -10,30 +10,24 @@ MODEL_LABELS = ['unacc', 'acc', 'vgood', 'good']
 @app.route('/predict')
 def predict():
     # Retrieve query parameters related to this request.
-    RevolvingUtilizationOfUnsecuredLines = request.args.get('RevolvingUtilizationOfUnsecuredLines')
-    age = request.args.get('age')
-    DebtRatio = request.args.get('DebtRatio')
-    MonthlyIncome = request.args.get('MonthlyIncome')
-    NumberOfOpenCreditLinesAndLoans = request.args.get('NumberOfOpenCreditLinesAndLoans')
-    NumberOfTimes90DaysLate = request.args.get('NumberOfTimes90DaysLate')
-    NumberRealEstateLoansOrLines = request.args.get('NumberRealEstateLoansOrLines')
-    NumberOfDependents = request.args.get('NumberOfDependents')
+    Buying = request.args.get('Buying')
+    Maint = request.args.get('Maint')
+    doors = request.args.get('doors')
+    lug_boot = request.args.get('lug_boot')
+    safety = request.args.get('safety')
 
     # Our model expects a list of records
-    features = [[RevolvingUtilizationOfUnsecuredLines, age, DebtRatio,
-                MonthlyIncome, NumberOfOpenCreditLinesAndLoans,
-                NumberOfTimes90DaysLate, NumberRealEstateLoansOrLines,
-                NumberOfDependents]]
+    features = [[Buying, Maint, doors, lug_boot, safety]]
 
 
-    # Use the model to predict the class
+    # predict the class and probability of the class
     label_index = MODEL.predict(features)
     label_conf = MODEL.predict_proba(features)
 
-    # Retrieve the iris name that is associated with the predicted class
+    # Retrieve the name of the predicted class
     label = MODEL_LABELS[label_index[0]]
-    # Create and send a response to the API caller
 
+    # Create and send a response to the API caller
     return jsonify(status='complete', label=label,  label_conf = ''.join(str(label_conf)))
 
 
